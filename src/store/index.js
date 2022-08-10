fb4f0af6621eaab9974e16d9942f0c4eaca2f96a
v_jiaxnchen <v_jiaxnchen@tencent.com>
2022-08-10 17:22:35 +0800

    取消dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,11 @@ import {createStore,applyMiddleware,compose} from 'redux'
 import thunk from 'redux-thunk'
 import reducer from './reducer'
 
-const  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace:true}) || compose
+// const  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({trace:true}) || compose
 
-const store = createStore(reducer,composeEnhancers(
-    applyMiddleware(thunk)
-))
+// const store = createStore(reducer,composeEnhancers(
+//     applyMiddleware(thunk)
+// ))
+const store = createStore(reducer,applyMiddleware(thunk))
 
 export default store
